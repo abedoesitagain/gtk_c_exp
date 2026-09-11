b14f63ce1ae2baa522d6395f1de9078e1f63d425
Ibrahim Al-Zubaidi <abe@dismail.de>
2026-09-11 22:46:40 +0100

    add Smith Numbers problem

--- sm/sm.c
+++ sm/sm.c
@@ -6,9 +6,76 @@
 #include <stdlib.h>
 #include <gtk/gtk.h>
 #include <ctype.h>
-#include <time.h>
 
 static GtkWidget *label[8] = { NULL };
+
+int digit_sum(int n){
+    char buf[10];
+    snprintf(buf, sizeof(buf), "%d", n);
+    int length=(int)strlen(buf);
+    int sum=0;
+    for (int i=0; i<length; i++) {
+        sum += buf[i] - '0';
+    }
+    return sum; 
+}
+
+int prime_factor(int n) {
+    int sum = 0;
+
+    if (n < 0) {
+        n = -n;
+    }
+
+    // Factor n and add the digits of each prime factor
+    for (int factor = 2; factor <= n / factor; factor++) {
+        while (n % factor == 0) {
+            int prime = factor;
+
+            while (prime > 0) {
+                sum += prime % 10;
+                prime /= 10;
+            }
+
+            n /= factor;
+        }
+    }
+
+    // Remaining prime factor
+    if (n > 1) {
+        int prime = n;
+
+        while (prime > 0) {
+            sum += prime % 10;
+            prime /= 10;
+        }
+    }
+
+    // Keep adding digits until only one digit remains
+    while (sum >= 10) {
+        int digit_sum = 0;
+
+        while (sum > 0) {
+            digit_sum += sum % 10;
+            sum /= 10;
+        }
+
+        sum = digit_sum;
+    }
+
+    printf("%d", sum);
+    return sum;
+}
+
+int is_smith(int n){
+    if (digit_sum(n)  == prime_factor(n)){
+        printf("%d is smith", n);
+        return 1;
+    }else{
+        printf("%d is not smith", n);
+        return 0;
+    }
+}
 
 int only_digits(const char *text) {
     if (*text == '\0') {
@@ -23,70 +90,19 @@ int only_digits(const char *text) {
 
     return 1;
 }
-int test(const char *string){
-    if (strlen(string) != 8){
-        return 0;
-    }else{
-        return only_digits(string);
-    }
-}
-
-void genrand(int arr[8], GtkWidget* label[8], GtkBox* box){
-    srand(time(0));
-    
-    for(int i=0;i<8;i++){
-        int randomNumber = 10000000 + rand() % 90000000;
-        arr[i]=randomNumber;
-        char prebuf[100];
-        snprintf(prebuf, sizeof(prebuf), "%d",randomNumber);
-        const char *buf = prebuf;
-        printf("%s\n", buf);
-        label[i] = gtk_label_new(buf);
-        gtk_box_append(GTK_BOX(box), label[i]);
-    }
-}
-
-void cleanrand(int arr[8], GtkWidget* label[8], GtkBox* box){
-    for(int i=0;i<8;i++){
-        if (label[i] != NULL){
-            gtk_box_remove(GTK_BOX(box), label[i]);
-            label[i] = NULL;
-        }
-    }
-}
 
 void on_clicked(GtkButton *button, gpointer user_data) {
     GtkEntry *entry = GTK_ENTRY(user_data);
     GtkLabel *output = GTK_LABEL(g_object_get_data(G_OBJECT(button), "output-label"));
     GtkBox *box = GTK_BOX(g_object_get_data(G_OBJECT(button), "box"));
-    int *ft = g_object_get_data(G_OBJECT(button), "ft");
-    printf("%d\n", *ft);
     char tempname[10];
     
     const char *text = gtk_editable_get_text(GTK_EDITABLE(entry));
-    if (test(text)){
-        gtk_label_set_text(output, "valid");
-        int randels[8];
-        // GtkWidget* label[8];
-        if (*ft == 0){
-            cleanrand(randels, label, box);
-        }
-        genrand(randels, label, box);
-        *ft = 0;
-        for(int i=0;i<8;i++){
-            char tempname[10];
-            snprintf(tempname, sizeof(tempname), "label-%d", i);
-            g_object_set_data(G_OBJECT(button), tempname, label[i]);
-        }
-        for(int i=0;i<8;i++){
-            const char textc = *text;
-            const char *otherc = gtk_label_get_text(GTK_LABEL(label[i]));
-            printf("comparing %s %s\n", text, otherc);
-            if (strcmp(text, otherc) == 0){
-                printf("yes\n");
-            }else{
-                printf("no\n");
-            }
+    if (only_digits(text)){
+        if(is_smith(atoi(text)) ){
+            gtk_label_set_text(output, "valid and smtih");
+        }else{
+            gtk_label_set_text(output, "valid and not smith");
         }
     }else{
         gtk_label_set_text(output, "not valid");
@@ -107,11 +123,9 @@ static void activate(GtkApplication *app, gpointer user_data) {
     gtk_box_append(GTK_BOX(box), entry);
     gtk_box_append(GTK_BOX(box), button);
     gtk_box_append(GTK_BOX(box), out_label);
-    int *ft = g_new(int, 1);
-    *ft = 1;
+
     g_object_set_data(G_OBJECT(button), "output-label", out_label);
     g_object_set_data(G_OBJECT(button), "box", box);
-    g_object_set_data(G_OBJECT(button), "ft", ft);
     g_signal_connect(button, "clicked", G_CALLBACK(on_clicked), entry);
     gtk_window_present(GTK_WINDOW(window));
 }
